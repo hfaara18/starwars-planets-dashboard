e6da8e31b0f61ac773b559e5f00d2d1668a7ccd9
hfaara18 <faarahussein@gmail.com>
2021-07-31 10:52:10 -0700

    successfully used fetch to retrieve planet data from API endpoint and stored barchart data in a state variable. Currently printing data to console, so use Chart.js to plot chart next

---
+++ src/common/Variables.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = "https://swapi.dev/api/"
+export const API_PLANETS_ENDPOINT = API_BASE_URL + "planets/"
--- src/components/layout/DashBoard.js
+++ src/components/layout/DashBoard.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+import { API_PLANETS_ENDPOINT } from '../../common/Variables';
 import BarChart from '../visualization/BarChart';
 import Table from '../visualization/Table';
 
-function DashBoard() {
-  return (
-    <div className="container">
-      <BarChart />
-      <Table />
-    </div>
-  )
+class DashBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      barChartData: [],
+    }
+  }
+
+  componentDidMount() {
+    this.getBarChartDataFromAPI(API_PLANETS_ENDPOINT);
+  }
+
+  async getBarChartDataFromAPI(endPoint) {
+    const responseData = await fetch(endPoint)
+        .then((response) => response.json())
+        .then((responseJson) => responseJson)
+        .catch((error) => console.error(error));
+
+    this.setState({
+      barChartData: [...this.state.barChartData, ...responseData.results.map((planetData) => {
+        const data = {
+        name: planetData.name,
+        population: planetData.population
+      }
+      return data;
+      })]
+    });
+
+    if (responseData.next != null) {
+      this.getBarChartDataFromAPI(responseData.next);
+    }
+  }
+
+
+  render() {
+    console.log(this.state.barChartData);
+    return (
+      <div className="container">
+        <div>
+          <h1>Starwars Planets Dashboard</h1>
+        </div>
+        <BarChart />
+        <Table />
+      </div>
+    )
+  }
 }
 
 export default DashBoard;
